d2d6eea6a3ea94dbfbe1d4d23efcba8bd706025b
ColdCipri <colder.ciprian@gmail.com>
2019-05-26 22:53:30 +0300

    Added CRUD Operations

---
+++ WP-Lab9/index.aspx
@@ -0,0 +1,148 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="WP_Lab9.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>
+        WP-Lab9 ASP.NET
+    </title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:HiddenField ID="hiddenfieldID" runat="server" />
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label Text="Name" runat="server"/>
+                    </td>
+                    <td colspan ="2">
+                        <asp:TextBox ID="textName" runat="server" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label Text="Username" runat="server"/>
+                    </td>
+                    <td colspan ="2">
+                        <asp:TextBox ID="textUsername" runat="server" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label Text="Age" runat="server"/>
+                    </td>
+                    <td colspan ="2">
+                        <asp:TextBox ID="textAge" runat="server" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label Text="Email" runat="server"/>
+                    </td>
+                    <td colspan ="2">
+                        <asp:TextBox ID="textEmail" runat="server" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label Text="Webpage" runat="server"/>
+                    </td>
+                    <td colspan ="2">
+                        <asp:TextBox ID="textWebpage" runat="server" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td colspan="2">
+                        <asp:Button Text="Save" ID="buttonSave" runat="server" OnClick="buttonSave_Click" />
+                        <asp:Button Text="Delete" ID="buttonDelete" runat="server" OnClick="buttonDelete_Click" />
+                        <asp:Button Text="Clear" ID="buttonClear" runat="server" OnClick="buttonClear_Click" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td colspan="2">
+                        <asp:Label Text="" ID="labelSuccessMessage" runat="server" Forecolor="Green" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td colspan="2">
+                        <asp:Label Text="" ID="labelErrorMessage" runat="server" Forecolor="Red" />
+                    </td>
+                </tr>
+            </table>
+            
+            <br />
+            <br />
+            <table>
+                <tr>
+                    <td colspan="2">
+                        <asp:Label ID="LabelSearch" runat="server" Text="Search By:"></asp:Label>
+                    </td>
+
+                    <td colspan="2">
+                        <asp:DropDownList ID="SearchList" runat="server">
+                            <asp:ListItem Selected="True">Name</asp:ListItem>
+                            <asp:ListItem>Username</asp:ListItem>
+                            <asp:ListItem>Age</asp:ListItem>
+                            <asp:ListItem>Email</asp:ListItem>
+                            <asp:ListItem>Webpage</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+
+                    <td>
+                        <asp:TextBox ID="textSearch" runat="server" OnTextChanged="textSearch_TextChanged"></asp:TextBox>
+                    </td>
+
+                </tr>
+            </table>
+            
+            
+            <br />
+            <br />
+
+            <asp:GridView ID="gvName" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="Name" HeaderText="Name" />
+                    <asp:BoundField DataField="Username" HeaderText="Username" />
+                    <asp:BoundField DataField="Age" HeaderText="Age" />
+                    <asp:BoundField DataField="Email" HeaderText="Email" />
+                    <asp:BoundField DataField="Webpage" HeaderText="Webpage" />
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:LinkButton Text="Select" ID="linkSelect" CommandArgument='<%# Eval("ID") %>' runat="server" OnClick="linkSelect_OnClick" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+            <br />
+            <br />
+            <p id="searched">Searched:</p>
+            <script type="text/javascript">
+                function copyText() {
+                    var searchedElem = document.getElementById("<%= SearchList.ClientID %>").value;
+                    searchedElem += " with ";
+                    searchedElem += document.getElementById("<%= textSearch.ClientID %>").value;
+                    var para = document.createElement("p");
+                    var node = document.createTextNode(searchedElem);
+                    para.appendChild(node);
+                    var element = document.getElementById("searched");
+                    element.appendChild(para);
+                }
+                copyText();
+            </script>
+        </div>
+    </form>
+
+</body>
+</html>
---
+++ WP-Lab9/index.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace WP_Lab9
+{
+    public partial class index : System.Web.UI.Page
+    {
+        string conn = @"Server=localhost;Database=usersdb;Uid=root;";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Clear();
+                GridFill();
+            }
+        }
+
+        protected void buttonSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection sqlCon = new MySqlConnection(conn))
+                {
+                    sqlCon.Open();
+                    MySqlCommand sqlCmd = new MySqlCommand("AddOrEditUser", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("_ID", Convert.ToInt32(hiddenfieldID.Value == "" ? "0" : hiddenfieldID.Value));
+                    sqlCmd.Parameters.AddWithValue("_Name", textName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("_Username", textUsername.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("_Age", Convert.ToInt32(textAge.Text.Trim()));
+                    sqlCmd.Parameters.AddWithValue("_Email", textEmail.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("_Webpage", textWebpage.Text.Trim());
+                    sqlCmd.ExecuteNonQuery();
+
+                    GridFill();
+                    Clear();
+
+                    labelSuccessMessage.Text = "Submitted Sucessfully!";
+                }
+            }
+            catch (Exception ex)
+            {
+                labelErrorMessage.Text = ex.Message;
+            }
+            
+        }
+
+        void Clear()
+        {
+            hiddenfieldID.Value = "";
+            textName.Text = textUsername.Text = textAge.Text = textEmail.Text = textWebpage.Text = "";
+            buttonSave.Text = "Save";
+            buttonDelete.Enabled = false;
+            labelErrorMessage.Text = labelSuccessMessage.Text = "";
+        }
+
+        protected void buttonClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        void GridFill()
+        {
+            using (MySqlConnection sqlCon = new MySqlConnection(conn))
+            {
+                sqlCon.Open();
+                MySqlDataAdapter sqlDataAdapter = new MySqlDataAdapter("UsersViewAll", sqlCon);
+                sqlDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                gvName.DataSource = dataTable;
+                gvName.DataBind();
+            }
+        }
+
+        protected void linkSelect_OnClick(object sender, EventArgs e)
+        {
+            int ID = Convert.ToInt32((sender as LinkButton).CommandArgument);
+
+            using (MySqlConnection sqlCon = new MySqlConnection(conn))
+            {
+                sqlCon.Open();
+                MySqlDataAdapter sqlDataAdapter = new MySqlDataAdapter("UsersViewByID", sqlCon);
+                sqlDataAdapter.SelectCommand.Parameters.AddWithValue("_ID", ID);
+                sqlDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                textName.Text = dataTable.Rows[0][1].ToString();
+                textUsername.Text = dataTable.Rows[0][2].ToString();
+                textAge.Text = dataTable.Rows[0][4].ToString();
+                textEmail.Text = dataTable.Rows[0][6].ToString();
+                textWebpage.Text = dataTable.Rows[0][7].ToString();
+
+                hiddenfieldID.Value = dataTable.Rows[0][0].ToString();
+
+                buttonSave.Text = "Update";
+                buttonDelete.Enabled = true;
+            }
+        }
+
+        protected void buttonDelete_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection sqlCon = new MySqlConnection(conn))
+            {
+                sqlCon.Open();
+                MySqlCommand sqlCmd = new MySqlCommand("UsersDeleteByID", sqlCon);
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("_ID", Convert.ToInt32(hiddenfieldID.Value == "" ? "0" : hiddenfieldID.Value));
+
+                sqlCmd.ExecuteNonQuery();
+
+                GridFill();
+                Clear();
+
+                labelSuccessMessage.Text = "Deleted Sucessfully!";
+            }
+        }
+
+        protected void textSearch_TextChanged(object sender, EventArgs e)
+        {
+            using (MySqlConnection sqlCon = new MySqlConnection(conn))
+            {
+                sqlCon.Open();
+                MySqlCommand sqlCmd = new MySqlCommand("SELECT * FROM user WHERE " + SearchList.Text.ToString() + " Like '" + textSearch.Text.ToString() + "%'", sqlCon);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.ExecuteNonQuery();
+                
+
+                MySqlDataAdapter sqlDataAdapter = new MySqlDataAdapter(sqlCmd.CommandText, sqlCon);
+                sqlDataAdapter.SelectCommand.CommandType = CommandType.Text;
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                gvName.DataSource = dataTable;
+                gvName.DataBind();
+
+                Clear();
+
+                ScriptManager.RegisterClientScriptBlock(this.Page, Page.GetType(), "uniqueScript", "copyText()", true);
+
+            }
+        }
+
+    }
+}
